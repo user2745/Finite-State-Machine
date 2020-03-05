f2001a83c53b857fc79d556c599cf9d156d144c1
Kevin Kamto <kamtokevin@gmail.com>
2020-03-04 23:55:23 -0500

    feat: added DFA

---
+++ DFA.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+// Example of a deterministic state machine
+
+public class DFA
+{
+
+    // States
+    private HashMap DFAMap;
+    private Object  state;
+    private Object transition;
+    // Path
+
+    DFA()
+    {
+        HashMap<Object, Object> DFAMap = new HashMap<>();
+        System.out.println("[Computer] DFA Initialized.");
+
+
+        
+    }
+
+    private void State(state, accepter);
+    // Everything runs here!
+    public static void main(String[] args) 
+    {
+        
+    }
+    //Hey what is up bro? I am alive!
+}
